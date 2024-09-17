72bbb899fef99e3fb4c2504a6decc33985e36371
Leela-Avinash <leelaavinash24@gmail.com>
2024-09-17 17:08:24 +0530

    resolve issues

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,8 +3,6 @@ import LandingPage from "./pages/Landingpage.jsx";
 import GetStarted from "./pages/GetStarted.jsx";
 // import RegistrationForm from "./pages/RegistrationForm.jsx"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import StudentReg from "./pages/StudentReg.jsx";
-import AlumniReg from "./pages/AlumniReg.jsx";
 import EmailVerify from "./pages/emailverify.jsx";
 import DocUpload from "./pages/docUpload.jsx";
 import Login from "./pages/Login.jsx";
@@ -17,8 +15,6 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<LandingPage />} />
                     <Route path="/getstarted" element={<GetStarted />} />
-                    <Route path="/stusignup" element={<StudentReg />} />
-                    <Route path="/alusignup" element={<AlumniReg />} />
                     <Route path="/:role/signup" element={<Registration />} />
                     <Route path="/users/:role/:id/verify/:token" element={<EmailVerify />} />
                     <Route path="/docai" element={<DocUpload/>}/>
