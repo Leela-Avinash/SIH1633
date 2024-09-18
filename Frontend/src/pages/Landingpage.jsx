2c3ffead39fc1ecb2fed8b31ce9f7a2c9790b2ea
CharanSala <salacharan81@gmail.com>
2024-09-18 16:56:46 +0530

    hello

--- Frontend/src/pages/Landingpage.jsx
+++ Frontend/src/pages/Landingpage.jsx
@@ -6,16 +6,19 @@ function LandingPage(){
   return(
     <div>
       <Navbar/>
-      <div>
-            <div className="bg-red-300">
-                <h1></h1>
+      <div className="flex justify-center items-center "> {/* Ensure full height to center vertically */}
+            <div className="grid grid-cols-2 pl-10 pr-10 rounded-lg">
+                <div className="bg-red-300 p-5 pl-10 pt-20">
+                    <h1 className="text-white text-xl font-bold text-2xl">Empower Your Alumni Network
+                    Foster Connections, Drive Engagement</h1>
+                    <p className="text-white mt-3 font-semibold text-md">A dynamic platform that enables alumni and students to stay connected. Foster meaningful interactions, professional development, and mentorships.</p>
+                </div>
+                <div className="flex justify-center items-center p-5">
+                    <img className="w-4/5" src="../public/bodylogo2.png" alt="Logo" />
+                </div>
             </div>
-          <div className=" flex justify-end mr-24">
-                  <img className=" w-5/12" src="../public/bodylogo2.png"></img>
-          </div>
-          
-       </div>
-    </div>
+        </div>
+      </div>
   )
 }
 export default LandingPage;
