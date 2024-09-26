2f46d43ac54825da5e3a1c49dc187f6355250640
J. PREMA SAGAR <premsagar.jonnalagadda@gmail.com>
2024-09-26 23:11:46 +0530

    Working on issues

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
 import { setUser } from "./redux/slices/userSlice.js";
-import { setAuth,setDoc } from "./redux/slices/authSlice.js";
+import { setAuth, setDoc } from "./redux/slices/authSlice.js";
 import LandingPage from "./pages/Landingpage.jsx";
 import GetStarted from "./pages/GetStarted.jsx";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
@@ -19,86 +20,96 @@ import ChatPage from "./pages/ChatPage.jsx";
 import Chatbot from "./components/Chatbot.jsx";
 import Otp from "./pages/Otp.jsx";
 const App = () => {
-    const dispatch = useDispatch();
-    const { isAuthenticated } = useSelector((state) => state.auth);
-    const { isDocVerified } = useSelector((state) => state.auth);
-    const user = useSelector((state) => state.user);
-    const users = [
-        {
-          id: 1,
-          name: 'John Doe',
-          jobTitle: 'Software Engineer',
-          profilePicture: 'https://via.placeholder.com/150',
-          skills: ['JavaScript', 'React', 'Node.js'],
-        },
-        {
-          id: 2,
-          name: 'Jane Smith',
-          jobTitle: 'Data Scientist',
-          profilePicture: 'https://via.placeholder.com/150',
-          skills: ['Python', 'Machine Learning', 'AI'],
-        },
-        {
-          id: 3,
-          name: 'Mike Johnson',
-          jobTitle: 'Product Manager',
-          profilePicture: 'https://via.placeholder.com/150',
-          skills: ['Leadership', 'Agile', 'Scrum'],
-        },
-        // Add more users...
-      ];
+  const dispatch = useDispatch();
+  const { isAuthenticated } = useSelector((state) => state.auth);
+  const { isDocVerified } = useSelector((state) => state.auth);
+  const user = useSelector((state) => state.user);
+  const [isChatOpen, setIsChatOpen] = useState(false);
+  const handleToggleChat = () => {
+    setIsChatOpen(!isChatOpen);
+  };
+  const users = [
+    {
+      id: 1,
+      name: 'John Doe',
+      jobTitle: 'Software Engineer',
+      profilePicture: 'https://via.placeholder.com/150',
+      skills: ['JavaScript', 'React', 'Node.js'],
+    },
+    {
+      id: 2,
+      name: 'Jane Smith',
+      jobTitle: 'Data Scientist',
+      profilePicture: 'https://via.placeholder.com/150',
+      skills: ['Python', 'Machine Learning', 'AI'],
+    },
+    {
+      id: 3,
+      name: 'Mike Johnson',
+      jobTitle: 'Product Manager',
+      profilePicture: 'https://via.placeholder.com/150',
+      skills: ['Leadership', 'Agile', 'Scrum'],
+    },
+    // Add more users...
+  ];
 
-    useEffect(() => {
-        const checkAuth = async () => {
-            try {
-                const response = await fetch(
-                    "http://localhost:5000/api/users/check-auth",
-                    {
-                        method: "GET",
-                        credentials: "include",
-                    }
-                );
-                const json = await response.json();
-                console.log(json);   
-                if (json.success) {
-                    localStorage.setItem("user", JSON.stringify(json.user));
-                    dispatch(setUser(json.user));
-                    dispatch(setAuth(true));
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await fetch(
+          "http://localhost:5000/api/users/check-auth",
+          {
+            method: "GET",
+            credentials: "include",
+          }
+        );
+        const json = await response.json();
+        console.log(json);
+        if (json.success) {
+          localStorage.setItem("user", JSON.stringify(json.user));
+          dispatch(setUser(json.user));
+          dispatch(setAuth(true));
 
-                    const storedUser = await JSON.parse(localStorage.getItem("user"));
-                    if (storedUser && storedUser.document_verification) {
-                        dispatch(setDoc(storedUser.document_verification))
-                    }
-                }
-            } catch (error) {
-                console.log("Error checking auth", error);
-            }
-        };
+          const storedUser = await JSON.parse(localStorage.getItem("user"));
+          if (storedUser && storedUser.document_verification) {
+            dispatch(setDoc(storedUser.document_verification))
+          }
+        }
+      } catch (error) {
+        console.log("Error checking auth", error);
+      }
+    };
 
-        checkAuth();
-    }, [dispatch]);
+    checkAuth();
+  }, [dispatch]);
 
-    return (
-        <div className="">
-            <Router>
-                <Routes>
-                    <Route path="/" element= {<LandingPage />} />
-                    <Route path="/getstarted" element={<GetStarted />} />
-                    <Route path="/:role/signup" element={isAuthenticated? (isDocVerified? <Navigate to="/dashboard" /> : <Navigate to="/docai"/>) : <Registration />} />
-                    <Route path="/users/:role/:id/verify/:token" element={<EmailVerify />} />
-                    <Route path="/docai" element={isAuthenticated ? (isDocVerified? <Navigate to="/dashboard" />: <DocUpload/>):<Navigate to="/login"/>}/>
-                    <Route path="/login" element={isAuthenticated ? (isDocVerified? <Navigate to="/dashboard" /> : <Navigate to="/docai"/>) : <Login/>}/>
-                    <Route path="/dashboard" element={isAuthenticated ? (isDocVerified ? <MainPage/> : <Navigate to="/docai" />) : <Navigate to="/login" />} />
-                    <Route path="/profile/:username" element={<Profile/> } />
-                    <Route path="/profilecompletion" element={<ProfileCompletionForm/>}/>
-                    <Route path="/network" element={<Network/>}/>
-                    {/* <Route path="/login" element={<Login/>}/> */}
-                    <Route path="/chat" element={isAuthenticated ? <ChatPage /> : <Navigate to={"/login"}/>} />
-                    <Route path="/otp" element={<Otp/>} />
-                </Routes>
-            </Router>
-        </div>
-    );
+  return (
+    <div className="">
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/getstarted" element={<GetStarted />} />
+          <Route path="/:role/signup" element={isAuthenticated ? (isDocVerified ? <Navigate to="/dashboard" /> : <Navigate to="/docai" />) : <Registration />} />
+          <Route path="/users/:role/:id/verify/:token" element={<EmailVerify />} />
+          <Route path="/docai" element={isAuthenticated ? (isDocVerified ? <Navigate to="/dashboard" /> : <DocUpload />) : <Navigate to="/login" />} />
+          <Route path="/login" element={isAuthenticated ? (isDocVerified ? <Navigate to="/dashboard" /> : <Navigate to="/docai" />) : <Login />} />
+          <Route path="/dashboard" element={isAuthenticated ? (isDocVerified ? <MainPage /> : <Navigate to="/docai" />) : <Navigate to="/login" />} />
+          <Route path="/profile/:username" element={<Profile />} />
+          <Route path="/profilecompletion" element={<ProfileCompletionForm />} />
+          <Route path="/network" element={<Network />} />
+          {/* <Route path="/login" element={<Login/>}/> */}
+          <Route path="/chat" element={isAuthenticated ? <ChatPage /> : <Navigate to={"/login"} />} />
+          <Route path="/otp" element={<Otp />} />
+        </Routes>
+        <button className="fixed bottom-3 left-5 p-4 rounded-full text-white focus:outline-none z-50"
+          onClick={handleToggleChat}
+        >
+          <img src="../chatbot1.gif" className="w-20 h-20"></img>
+        </button>
+        <Chatbot isOpen={isChatOpen} handleToggle={handleToggleChat} />
+      </Router>
+    </div>
+  );
 };
 
 export default App;
